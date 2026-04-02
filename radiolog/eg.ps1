8ae328427ec82a0bd445fed2b513b143dd2e7679
Jeongrae Kim <102293576+Jeong-Rae@users.noreply.github.com>
2026-04-02 11:31:44 +0000

    feat(radiolog): 헬스 체크 라우트 제거 및 시작 로직 간소화

--- radiolog/eg.ps1
+++ radiolog/eg.ps1
@@ -8,19 +8,6 @@ Import-Module $modulePath -Force
 
 $app = New-App -Root $PSScriptRoot -Name "radiolog"
 
-Add-Route $app GET "/health" {
-    param($req, $res)
-    $null = $res.Ok(@{ status = "ok" })
-}
-
 Use-Static $app "/" "./" -DefaultDocument @("index.html")
 
-Write-Host ("[radiolog] serving on http://{0}:{1}/" -f $BindHost, $Port)
-if ($BindHost -eq "localhost") {
-    Start-App -App $app -Prefix @(
-        ("http://+:{0}/" -f $Port)
-    )
-}
-else {
-    Start-App -App $app -Port $Port -BindHost $BindHost
-}
+Start-App -App $app -Prefix @(("http://+:{0}/" -f $Port))
